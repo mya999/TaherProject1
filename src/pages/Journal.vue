149b7817544ba671b2d27a54dd52cb127300a2bb
mya9 <imohduni@gmail.com>
2020-04-19 21:55:28 +0300

    update

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="journal-hero">
         <h1 class="journal-header">
-          a wise person once said...
+Here we post our information
         </h1>
       </div>
     </div>
